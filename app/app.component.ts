e38243a88f8c7da56e4d98eba838f8dbb20cee9d
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-20 17:12:00 -0700

    setup intial file structure

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Album } from './album.model';
+import { Cart } from './cart.model';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Health Tracker</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+  
+}
